7b326f885df7650a7f6fa706b3be0700d38ca194
Courtney Tipps <tippy2.tenor@gmail.com>
2021-05-16 15:38:23 -0400

    So many changes

--- KnockKnockJokes/Program.cs
+++ KnockKnockJokes/Program.cs
@@ -9,9 +9,9 @@ namespace KnockKnockJokes
         {
             JokeList jokes = new JokeList();
             Random rand = new Random();
-
-            int jokeNumber = rand.Next(0, jokes.opener.Count - 1);
             bool mistake = false;
+            bool hearAJoke = true;
+
             Console.Write("Would you like to hear a Knock Knock Joke? Y/N : ");
             string answer = Console.ReadLine().ToUpper();
             if (answer == "Y")
@@ -27,30 +27,44 @@ namespace KnockKnockJokes
             {
                 Console.WriteLine("I'm not sure what you said, but I think you want to hear one.");
             }
-            Console.WriteLine("Knock Knock");
-            string response = Console.ReadLine().ToLower();
-            if (response != "who's there?" && response != "who's there")
+            while (hearAJoke)
             {
-                mistake = true;
-                Console.WriteLine(" That's not the right response. It's 'Who's there?'. But that's okay, let's continue.");
-            }
-            Console.WriteLine(jokes.opener[jokeNumber]);
-            string prePunch = Console.ReadLine().ToLower();
+                int jokeNumber = rand.Next(0, jokes.opener.Count - 1);
+                Console.WriteLine("Knock Knock");
+                string response = Console.ReadLine().ToLower();
+                if (response != "who's there?" && response != "who's there")
+                {
+                    mistake = true;
+                    Console.WriteLine(" That's not the right response. It's 'Who's there?'. But that's okay, let's continue.");
+                }
+                Console.WriteLine(jokes.opener[jokeNumber]);
+                string prePunch = Console.ReadLine().ToLower();
 
 
-            if (prePunch != jokes.opener[jokeNumber].ToLower() + " who" && prePunch != jokes.opener[jokeNumber].ToLower() + " who?")
-            {
-                if (mistake)
+                if (prePunch != jokes.opener[jokeNumber].ToLower() + " who" && prePunch != jokes.opener[jokeNumber].ToLower() + " who?")
                 {
-                    Console.WriteLine("Wow, you're really bad at knock knock jokes! You're supposed to say '" + jokes.opener[jokeNumber] + " who?'. Then I say:");
+                    if (mistake)
+                    {
+                        Console.WriteLine("Wow, you're really bad at knock knock jokes! You're supposed to say '" + jokes.opener[jokeNumber] + " who?'. Then I say:");
+                    }
+                    else
+                    {
+                        Console.WriteLine("That's not right, it's: " + jokes.opener[jokeNumber] + " who? Then I say:");
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("That's not right, it's: " + jokes.opener[jokeNumber] + " who? Then I say:");
-                }
-            }
                 Console.WriteLine(jokes.GetPunchline(jokeNumber));
-
+                Console.WriteLine("");
+                Console.Write("Would you like to hear another? Y/N: ");
+                string contResponse = Console.ReadLine().ToUpper();
+                if (contResponse == "Y")
+                {
+                    hearAJoke = true;
+                } else
+                {
+                    Console.WriteLine("That's a shame!");
+                    hearAJoke = false;
+                }
+            }
                 Console.Write("Hit any key to exit.");
                 Console.ReadLine();
 
